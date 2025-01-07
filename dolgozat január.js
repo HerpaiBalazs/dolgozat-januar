4fdf6730cfcc760bc203ad05f91af69ad1db5280
Herpai Balazs <116707173+HerpaiBalazs@users.noreply.github.com>
2025-01-07 13:12:16 +0100

    Update dolgozat január.js

--- dolgozat január.js
+++ dolgozat január.js
@@ -17,8 +17,8 @@ function getCount(str) {
     for (let i = 2; i < integer; i++) {
       if (integer % i === 0) { 
         result.push(i); 
-    } 
+        } 
     } 
     return result.length ? result : `${integer} is prime`; 
-  }
+    }
   
